ad1450c9ad284f44ae8fa07429c3900222de4c4f
Arantxa Morcillo <arantxa.morcillo@gmail.com>
2022-01-20 15:21:05 +0100

    maven refactor

--- src/main/java/com/bosonit/arantxamorcillo/Application.java
+++ src/main/java/com/bosonit/arantxamorcillo/Application.java
@@ -1,10 +1,7 @@
 package com.bosonit.arantxamorcillo;
-
 import java.io.BufferedReader;
-
 import java.io.FileReader;
 import java.io.IOException;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
